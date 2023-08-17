92d58b995f5d158d7fdecec9f851d24217588a5b
Iriogbe Peter <iriogbepeter22@gmail.com>
2023-08-17 10:09:13 +0100

    forget password api logic moved to services

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,7 @@ Route::prefix('v1')->group(function () {
     Route::apiResource('product', ProductController::class)->only(['index', 'show']);
     Route::apiResource('productCategory', ProductCategoryController::class)->only(['index', 'show']);
 
+    // authentication routes
     Route::prefix('auth')->group(function () {
         Route::post('register', [RegisterController::class, 'register']);
         Route::post('login', [LoginController::class, 'login']);
